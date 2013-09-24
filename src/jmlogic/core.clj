df968736f50e387ac912f27219d182b361a3cc12
Juan M. Hernandez <juan.hernandez.arroyo@gmail.com>
2013-09-24 22:00:29 +0200

    intentando hacer un project para las diagonales

--- src/jmlogic/core.clj
+++ src/jmlogic/core.clj
@@ -3,83 +3,52 @@
   (:use clojure.core.logic)
   (:require [clojure.core.logic.fd :as fd]))
 
-(defne nonmembero
-  "Comprueba si un elemento es miembro de una colección"
-  [x l]
-  ([_ ()])
-  ([_ [head . tail]]
-     (!= x head)
-     (nonmembero x tail)))
-
-(defne alldistincto
-  "Comprueba si todos los elementos de una lista degenerada de esas son distintos"
-  [l]
-  ([()]) ; Cuando no hay nada, son todos distintos
-  ([[head . tail]]
-   (nonmembero head tail)
-   (alldistincto tail)))
-
-
-(run* [q]
-      (alldistincto [1 2 673 3])
-      (== q 1))
-
-; Esto funciona y unifica sobre la misma lista
-(defne idento
-  [xs ys]
-  ([() ()])
-  ([[h1 . t1] [h2 . t2]]
-     (== h1 h2)
-     (idento t1 t2)))
-
 
 (def queens (repeatedly 8 lvar))
 (def domain (fd/domain 1 2 3 4 5 6 7 8))
 
-(defne rowo
-  "unifica la fila"
-  [queen r]
-  ([[?row _] ?row]))
-
-(defne columno
-  "unifica la columna"
-  [queen c]
-  ([[_ ?col] ?col]))
-
-
 (defne rowso
   "unifica un vector de posiciones, expresadas como vectores [row column] con el vector formado por sus filas"
   [queens rows]
-  ; Si no hay ninguna reina, no hay ninguna fila
-  ([() ()])
-  ; Si hay cosas, tomamos la fila de la primera y la concatenamos con
-  ; las filas del resto
-  ([[[?r _] . tail] [?r . ?rs]]
+  ([() ()]) ; caso base. No hay reinas, no hay filas
+  ([[[?r _] . tail] [?r . ?rs]] ; la fila de la 1ª con las filas del resto
      (rowso tail ?rs)))
 
-(run* [q]
-      (rowso [[1 2] [5 6] [8 15] [6 1]] q))
+(defne colso
+  "unifica un vector de posiciones, expresadas como vectores [row column] con el vector formado por sus columnas"
+  [queens columns]
+  ([() ()]) ; caso base. No hay reinas, no hay columnas
+  ([[[_ ?c] . tail] [?c . ?cs]] ; la fila de la 1ª con las columnas del resto
+     (colso tail ?cs)))
 
+(defne diags1o
+  "diagonales de arriba-izda a abajo dcha"
+  [queens diags]
+  ([() ()])
+  ([[[?r ?c] . tail] [?d1 . ?ds]]
+     (project [?r ?c ?d1]
+              (== (- ?r ?c) ?d1))
+     (diags1o tail ?ds)))
 
-
-(run* [q]
-      (fresh [r c]
-             (rowo [5 11] r)
-             (columno [56 19] c)
-             (== q [r c])))
-
-; OK, ahora a ver si consigo usar el fd/distinct. No puedo porque rows
-; no está en un dominio finito, pero puedo usar distincto (menos
-; eficiente, eso sí)
-
+(defne diag1o [queen diag]
+  ([[r c] d]
+     (project [r c d]
+              (== (- r c) d))))
 
 (run 1 [q]
-     (fresh [rows cols]
+     (diag1o [1 5] q))
+
+(run 1 [q]
+     (fresh [rows cols diags1 diags2]
             (everyg (fn [queen]
                       (fresh [r c]
                              (== [r c] queen)
                              (fd/in r domain)
                              (fd/in c domain))) queens)
             (rowso queens rows)
-            (distincto rows))
+            (distincto rows)
+            (colso queens cols)
+            (distincto cols)
+            (diags1o queens diags1)
+            (distincto diags1))
      (== q queens))
