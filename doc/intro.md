165fc9bfebc1d4dce5aca39a58e07e91bb561a9c
jmhdez <juan.hernandez.arroyo@gmail.com>
2013-09-24 16:47:03 +0200

    Initial source commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to jmlogic
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ src/jmlogic/core.clj
@@ -0,0 +1,47 @@
+(ns jmlogic.core
+  (:refer-clojure :exclude [==])
+  (:use clojure.core.logic)
+  (:require [clojure.core.logic.fd :as fd]))
+
+(defne nonmembero
+  "Comprueba si un elemento es miembro de una colección"
+  [x l]
+  ([_ ()])
+  ([_ [head . tail]]
+     (!= x head)
+     (nonmembero x tail)))
+
+(defne alldistincto
+  "Comprueba si todos los elementos de una lista degenerada de esas son distintos"
+  [l]
+  ([()]) ; Cuando no hay nada, son todos distintos
+  ([[head . tail]]
+   (nonmembero head tail)
+   (alldistincto tail)))
+
+
+
+(run* [q]
+      (alldistincto [1 2 673 3])
+      (== q 1))
+
+; Voy a intentar crear dos variables y enlazarlas a ver si lo consigo
+(def rows (repeatedly 8 lvar))
+(def columns (repeatedly 8 lvar))
+
+(def queens (repeatedly 8 lvar))
+
+(def domain (fd/domain 1 2 3 4 5 6 7 8))
+
+(run 4 [q]
+     (== q [rows columns])
+     (everyg #(fd/in % domain) rows)
+     (everyg #(fd/in % domain) columns))
+
+(run 4 [q]
+     (== q [queens])
+     (everyg (fn [queen]
+               (fresh [r c]
+                      (== [r c] queen)
+                      (fd/in r domain)
+                      (fd/in c domain))) queens))
